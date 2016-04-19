e6e9e82efcf5826f712b08aa3a70a672d3ec6b7a
loveky <ylzcylx@gmail.com>
2016-04-19 13:12:41 +0800

    展示tab组件元素，hover时滑入视区

--- src/backend.js
+++ src/backend.js
@@ -18,5 +18,49 @@ const bridge = new Bridge({
 });
 
 bridge.on('init', () => {
-	bridge.send('info', {switchable: $('*').length});
+	bridge.send('info', gatherComponments());
 });
+
+bridge.on('focus', (data) => {
+	var ele = getElement(data.type, data.expando);
+	$('html, body').animate({
+	    scrollTop: ele.offset().top -20
+	});
+})
+
+// Helper
+const expando = jQuery.expando;
+function gatherComponments () {
+	var tabs = [];
+
+	for (let tab of $.ui.all().tab) {
+		tabs.push({
+			expando: tab.el[0][expando],
+			options: cloneOptions(tab.cache('options')),
+			selector: tab.el.selector
+		})
+	}
+
+	return tabs;
+}
+
+function cloneOptions (options) {
+	var result = {};
+	for (let key of Object.keys(options)) {
+		if ((typeof options[key]).match(/boolean|number|string/)) {
+			result[key] = options[key];
+		}
+	}
+
+	return result;
+}
+
+function getElement (type, expandoValue) {
+	for (let instance of $.ui.all()[type]) {
+		if (instance.el[0][expando] == expandoValue) {
+			return instance.el;
+		}
+	}
+}
+
+window.getElement = getElement;
--- src/devtoolsApp.js
+++ src/devtoolsApp.js
@@ -1,5 +1,35 @@
+let UI_INSTANCE_TEMPLATE = '{for instance in list}'
+							+'<div data-expando="${instance.expando}" class="ui-instance">'
+							+'	<h2 class="selector">${instance.selector}</h2>'
+							+'	<div class="operations">'
+							+'		<a data-expando="${instance.expando}" class="inspect" href="#">审查元素</a>'
+							+'		<a data-expando="${instance.expando}" class="debug" href="#">调试参数</a>'
+							+'	</div>'
+							+'</div>'
+							+'{/for}'
+
 export function initDevtoolsApp (bridge) {
 	bridge.on('info', function (payload) {
-       $('#switchable').html(payload.switchable);
+       $('body').html(UI_INSTANCE_TEMPLATE.process({list:payload}));
+	});
+	
+	$('body').on('mouseenter', '.ui-instance', function () {
+		$(this).addClass('hover');
+		bridge.send('focus', {
+			type: 'tab',
+			expando: $(this).data('expando')
+		})
+	});
+
+	$('body').on('mouseleave', '.ui-instance', function () {
+		$(this).removeClass('hover');	
+	});
+	
+	$('body').on('click', '.inspect', function () {
+		chrome.devtools.inspectedWindow.eval('inspect(getElement("tab", ' + $(this).data('expando') + ')[0])');
 	})
+
 }
+
+
+
